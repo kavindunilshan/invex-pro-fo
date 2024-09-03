27a784f7b23c8b18e5669b775c5c40cefd2e5fd8
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-09-03 17:43:34 +0530

    Content added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/home/navbar/navbar.component';
+import { NavbarComponent } from './components/home/home-box/navbar/navbar.component';
 import { ActionButtonComponent } from './utils/action-button/action-button.component';
 import { HomeComponent } from './components/home/home.component';
+import { HomeBoxComponent } from './components/home/home-box/home-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ActionButtonComponent,
-    HomeComponent
+    HomeComponent,
+    HomeBoxComponent
   ],
   imports: [
     BrowserModule,
