adc02f857b5d3878a88f541ce57d95d7b474ed47
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-09-03 13:10:08 +0530

    Home component created and navbar moved into it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './components/home/navbar/navbar.component';
 import { ActionButtonComponent } from './utils/action-button/action-button.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ActionButtonComponent
+    ActionButtonComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
