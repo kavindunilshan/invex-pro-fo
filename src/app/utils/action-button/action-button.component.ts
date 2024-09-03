5e4c4cdf3394dd6a03fa590aadb4662286f8ac9d
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-09-03 12:57:35 +0530

    Content added

--- src/app/utils/action-button/action-button.component.ts
+++ src/app/utils/action-button/action-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-action-button',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './action-button.component.css'
 })
 export class ActionButtonComponent {
-
+  @Input() text: string = 'Click Here';
 }
