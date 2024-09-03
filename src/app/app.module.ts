24b0c16c6115debbe37258821afeaa4c7e0b5554
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-09-03 12:45:12 +0530

    Navbar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ActionButtonComponent } from './utils/action-button/action-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ActionButtonComponent
   ],
   imports: [
     BrowserModule,
