0857f9a7a9fdbc18fa4f9552cfa12938ff820051
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-09-10 21:52:32 +0530

    All form components done.

--- src/app/components/action-center/management/category/category.component.ts
+++ src/app/components/action-center/management/category/category.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { MatDialog } from "@angular/material/dialog";
+import { DynamicFormDialogComponent } from "../../general/dynamic-form-dialog/dynamic-form-dialog.component";
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+  styleUrls: ['./category.component.css']
 })
 export class CategoryComponent {
 
+  constructor(private dialog: MatDialog) {}
+
+  openCategoryForm() {
+    const categoryFormConfig = [
+      { label: 'Category Name', name: 'name', type: 'text', validators: ['required'] },
+      { label: 'Description', name: 'description', type: 'text', validators: ['required'] }
+    ];
+
+    const dialogRef = this.dialog.open(DynamicFormDialogComponent, {
+      width: '600px',
+      height: 'auto',
+      data: {
+        formConfig: categoryFormConfig,
+        title: 'Add New Category'
+      },
+      panelClass: 'custom-dialog-container',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Category data:', result);
+      }
+    });
+  }
 }
--- src/app/components/action-center/management/supplier/supplier.component.ts
+++ src/app/components/action-center/management/supplier/supplier.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { MatDialog } from "@angular/material/dialog";
+import { DynamicFormDialogComponent } from "../../general/dynamic-form-dialog/dynamic-form-dialog.component";
 
 @Component({
   selector: 'app-supplier',
   templateUrl: './supplier.component.html',
-  styleUrl: './supplier.component.css'
+  styleUrls: ['./supplier.component.css']
 })
 export class SupplierComponent {
 
+  constructor(private dialog: MatDialog) {}
+
+  openSupplierForm() {
+    const supplierFormConfig = [
+      { label: 'Supplier Name', name: 'supplierName', type: 'text', validators: ['required'] },
+      { label: 'Contact Name', name: 'contactName', type: 'text', validators: ['required'] },
+      { label: 'Address', name: 'address', type: 'text', validators: ['required'] },
+      { label: 'City', name: 'city', type: 'text', validators: ['required'] },
+      { label: 'Postal Code', name: 'postalCode', type: 'text', validators: ['required'] },
+      { label: 'Country', name: 'country', type: 'text', validators: ['required'] },
+      { label: 'Phone', name: 'phone', type: 'text', validators: ['required'] },
+      { label: 'Email', name: 'email', type: 'text', validators: ['required'] }
+    ];
+
+    const dialogRef = this.dialog.open(DynamicFormDialogComponent, {
+      width: '600px',
+      height: 'auto',
+      data: {
+        formConfig: supplierFormConfig,
+        title: 'Add New Supplier'
+      },
+      panelClass: 'custom-dialog-container',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Supplier data:', result);
+      }
+    });
+  }
 }
