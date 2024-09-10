f695759035d07a7a0c115391b37a5862a910922c
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-09-10 20:28:16 +0530

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import {HomeComponent} from "./components/home/home.component";
 import {ActionCenterComponent} from "./components/action-center/action-center.component";
 import {DashboardComponent} from "./components/action-center/dashboard/dashboard.component";
 import {ProductComponent} from "./components/action-center/management/product/product.component";
+import {CategoryComponent} from "./components/action-center/management/category/category.component";
+import {CustomerComponent} from "./components/action-center/management/customer/customer.component";
+import {SupplierComponent} from "./components/action-center/management/supplier/supplier.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -11,7 +14,12 @@ const routes: Routes = [
   { path: 'action-center', component: ActionCenterComponent,
     children: [
       { path: 'dashboard', component: DashboardComponent },
-      { path: 'products', component: ProductComponent }]
+      { path: 'products', component: ProductComponent },
+      { path: 'categories', component: CategoryComponent },
+      { path: 'customers', component: CustomerComponent },
+      { path: 'suppliers', component: SupplierComponent},
+
+    ]
   }
 ];
 
