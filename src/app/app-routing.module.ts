c144b55133ddfb4cc37ad15a9c14e7ab510b7c2b
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-09-27 17:05:02 +0530

    Routing added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,10 @@ import {CategoryComponent} from "./components/action-center/management/category/
 import {CustomerComponent} from "./components/action-center/management/customer/customer.component";
 import {SupplierComponent} from "./components/action-center/management/supplier/supplier.component";
 import {NotificationComponent} from "./components/action-center/menu/notification/notification.component";
+import {OrdersComponent} from "./components/action-center/transactions/orders/orders.component";
+import {PurchaseComponent} from "./components/action-center/transactions/purchase/purchase.component";
+import {ShipmentsComponent} from "./components/action-center/transactions/shipments/shipments.component";
+import {ReturnsComponent} from "./components/action-center/transactions/returns/returns.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -15,12 +19,17 @@ const routes: Routes = [
   { path: 'action-center', component: ActionCenterComponent,
     children: [
       { path: 'dashboard', component: DashboardComponent },
+      { path: 'notifications', component: NotificationComponent },
+
       { path: 'products', component: ProductComponent },
       { path: 'categories', component: CategoryComponent },
       { path: 'customers', component: CustomerComponent },
       { path: 'suppliers', component: SupplierComponent},
 
-      { path: 'notifications', component: NotificationComponent },
+      { path: 'orders', component: OrdersComponent },
+      { path: 'purchases', component: PurchaseComponent },
+      { path: 'shipments', component: ShipmentsComponent },
+      { path: 'returns', component: ReturnsComponent },
 
     ]
   }
