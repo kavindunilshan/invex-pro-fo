fe0e592789e21b37587fcb3ba9b9b78e9e8d2407
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-10-04 15:41:31 +0530

    Config added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import {provideAuth0} from "@auth0/auth0-angular";
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
+  ngZoneEventCoalescing: true,
+  providers: [
+    provideAuth0({
+      domain: 'dev-c5ls7veng3ljfc5g.us.auth0.com',
+      clientId: 'BrXpQ8T5kbczgBXZDujxeuMfeBnuiQeg',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+  ]
 })
   .catch(err => console.error(err));
